abaa3c123dda845b91f0dffa3a5d013f76e89ce1
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2021-09-06 22:36:04 +0200

    add database configuration for using postgresql to setting file

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'crm_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crm',
+        'USER':  'nasredine',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

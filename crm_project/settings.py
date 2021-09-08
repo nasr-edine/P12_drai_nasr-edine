140c979e03be6aa01943ed1ebd9fbfdd330261eb
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2021-09-08 13:26:43 +0200

    separate new local apps from apps installed by default

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local
     'customerapp',
+    'contractapp',
 ]
 
 MIDDLEWARE = [

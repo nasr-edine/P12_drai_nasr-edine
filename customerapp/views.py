4f07dddbdec3db2a6bd0810810fe45dfe476b6f6
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2021-09-08 14:42:52 +0200

    add first route view and template for model customer

--- customerapp/views.py
+++ customerapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import Customer
+
+
+class CustomerListView(ListView):
+    model = Customer
+    template_name = 'customers/customer_list.html'

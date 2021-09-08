60d5cb12fd1cdc450b9bfc891b87649594cfcd85
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2021-09-08 17:10:53 +0200

    add first test for customer model

--- customerapp/tests.py
+++ customerapp/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
+from customerapp.models import Customer
+
 # Create your tests here.
+
+
+class CustomerModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Customer.objects.create(
+            first_name='first customer', last_name='a body here')
+
+    def test_name_content(self):
+        customer = Customer.objects.get(customer_id=1)
+        expected_object_name = f'{customer.first_name}'
+        self.assertEquals(expected_object_name, 'first customer')

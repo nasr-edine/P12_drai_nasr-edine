d0e0568c59a8fe5a638b26f13f1d76d58e23adb9
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2021-09-16 09:21:02 +0200

    custom admin site for staff model

--- backend/staff/admin.py
+++ backend/staff/admin.py
@@ -1,5 +1,44 @@
 from django.contrib import admin
+
 from staff.models import Member
+from django.contrib.auth.models import Group
+
 
 # Register your models here.
-admin.site.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'email',
+                    'role')
+    fieldsets = (
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Contact', {'fields': ('email', 'phone', 'mobile')}),
+        ('security', {'fields': ('username', 'password')}),
+        ('Permissions', {'fields': ('role',)}),
+    )
+    radio_fields = {'role': admin.HORIZONTAL}
+
+    ordering = ['-date_created']
+
+    list_filter = ['role']
+
+    readonly_fields = ('date_created',)
+
+    def save_model(self, request, obj, form, change):
+        obj.is_staff = True
+        obj.set_password(obj.password)
+        mgmt_group = Group.objects.get(name='management')
+        sales_group = Group.objects.get(name='sales')
+        support_group = Group.objects.get(name='support')
+        no_privilege = Group.objects.get(name='no_privilege')
+
+        obj.save()
+        if obj.role == 'sales':
+            obj.groups.add(sales_group)
+        elif obj.role == 'support':
+            obj.groups.add(support_group)
+        elif obj.role == 'management':
+            obj.groups.add(mgmt_group)
+        else:
+            obj.groups.add(no_privilege)
+
+
+admin.site.register(Member, MemberAdmin)

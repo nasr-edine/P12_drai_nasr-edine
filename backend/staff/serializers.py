c371d0604933252e6679c2abbf1b10a0f4867ede
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2021-09-16 09:22:37 +0200

    update serializers

--- backend/staff/serializers.py
+++ backend/staff/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from .models import Member
+from django.contrib.auth.hashers import make_password
 
 
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
-        fields = ('id', 'username', 'first_name', 'last_name', 'email',
-                  'phone', 'mobile', 'date_created', 'role')
+        fields = ('id', 'email', 'password', 'first_name',
+                  'last_name', 'phone', 'mobile', 'date_created', 'role')
+
+        # extra_kwargs = {
+        #     'password': {'write_only': True},
+        # }

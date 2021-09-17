53231f2803ec4e0bc40d6fb5b3b67cc99e19af97
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2021-09-17 13:42:34 +0200

    add field is_staff and is_superuser in serializer

--- backend/staff/serializers.py
+++ backend/staff/serializers.py
@@ -6,8 +6,8 @@ from django.contrib.auth.hashers import make_password
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
-        fields = ('id', 'email', 'password', 'first_name',
-                  'last_name', 'phone', 'mobile', 'date_created', 'role')
+        fields = ('id', 'username', 'email', 'password', 'first_name',
+                  'last_name', 'phone', 'mobile', 'date_created', 'role', 'is_staff', 'is_superuser')
 
         # extra_kwargs = {
         #     'password': {'write_only': True},
